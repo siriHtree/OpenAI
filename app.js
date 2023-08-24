79e3e88dd8c4d5878c2160b43f8ee4d918da426d
Moulsiri Awasthi <moulsiri.awasthi@htree.plus>
2023-08-24 15:21:23 +0530

    made another request with gpt-3.5-turbo model

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const API_key="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"
+
 async function fetchData(){
     const response=await fetch("https://api.openai.com/v1/completions",{
         method:"POST",
@@ -17,17 +17,28 @@ async function fetchData(){
     console.log(data)
 }
 
+/**
+ * Chat completion
+ */
 async function fetchDataChat(){
-    const response=await fetch("https://api.openai.com/v1/completions",{
+    const response=await fetch("https://api.openai.com/v1/chat/completions",{
         method:"POST",
         headers:{
             Authorization: `Bearer ${API_key}`,
             "Content-Type":"application/json"
         },
         body:JSON.stringify({
-            model:"text-davinci-003",
-            prompt:"hello, how are you today?",
-            max_tokens:7,
+            model:"gpt-3.5-turbo",
+            messages:[
+                {
+                    role:"user",
+                    content:"Hello!"
+                },
+                {
+                    role:"user",
+                    content:"How are you?"
+                }
+            ]
         })
     })
     const data=await response.json();
